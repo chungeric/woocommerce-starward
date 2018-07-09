7bd0698254eaee7db8f23b4825831acf2cd1ebfe
Eric Chung <rchung.eric@gmail.com>
2018-07-09 17:41:51 +1000

    Added react-slider price slider, filter by price functional

--- app/components/Products/LayeredNavigation/PriceSlider.jsx
+++ app/components/Products/LayeredNavigation/PriceSlider.jsx
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import ReactSlider from 'react-slider';
 
 class PriceSlider extends Component {
+  handleChange = (value) => {
+    const { location } = this.props;
+    const newMinPrice = value[0];
+    const newMaxPrice = value[1];
+    console.log('location', location);
+    browserHistory.push({
+      ...location,
+      query: {
+        ...location.query,
+        min_price: newMinPrice,
+        max_price: newMaxPrice
+      }
+    });
+  }
+
   render() {
-    const { filter, index } = this.props;
+    const { filter, index, location } = this.props;
     const { min_price: minPrice, max_price: maxPrice } = filter;
+    const hasNewPrices = ('min_price' in location.query) || ('max_price' in location.query);
+    const newMinPrice = hasNewPrices ? parseInt(location.query.min_price) : null;
+    const newMaxPrice = hasNewPrices ? parseInt(location.query.max_price) : null;
+    console.log(location);
+    console.log(newMinPrice);
+    console.log(newMaxPrice);
     if (filter && minPrice !== maxPrice) {
       return (
         <section className="filter-block" key={index}>
           <h3>Price</h3>
-          <label htmlFor="price">Price</label>
-          <input
-            type="range"
-            id="price-slider"
-            name="price"
+          <ReactSlider
+            defaultValue={hasNewPrices ? [newMinPrice, newMaxPrice] : [minPrice, maxPrice]}
             min={minPrice}
             max={maxPrice}
+            className="price-slider"
+            onAfterChange={this.handleChange}
+            withBars
             />
+          <p>Testing</p>
         </section>
       );
     }
